427f7b6769cefa50a818f186f2e54790c0002b97
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-12-12 18:01:30 -0500

    fixup that api

--- api/handlers.py
+++ api/handlers.py
@@ -7,6 +7,16 @@ class LegislatorHandler(BaseHandler):
               'district','state','party','gender','currently_serving',
               'govtrack_id','bioguide_id','display_name')
     allowed_methods = ('GET',)
+    
+    def read(self, request):
+        qs = Legislator.objects.all()
+        if 'state' in request.GET:
+            qs = qs.filter(state=request.GET['state'].upper())
+        if 'party' in request.GET:
+            qs = qs.filter(party=request.GET['party'].upper())
+        if 'currently_serving' in request.GET:
+            qs = qs.filter(currently_serving=request.GET['currently_serving'] != '0')
+        return qs
 
 class LegislationHandler(BaseHandler):
     model = Legislation
